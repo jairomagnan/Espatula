1ca20c9a65656b36ce5afbc90e62103d9a49b5b0
jesustq <jetoqu91@gmail.com>
2015-07-17 16:45:41 -0600

    d

--- Espatula/AgregarReceta.aspx.cs
+++ Espatula/AgregarReceta.aspx.cs
@@ -73,13 +73,7 @@ namespace Espatula {
 
                     con.Close();
 
-                    con.Open();
-
-                    for (int i = 0; i < ingrediente.Length;++i) {
-                        MySqlCommand cmd3 = new MySqlCommand("INSERT INTO ingredientes (nombre) VALUES('" + ingrediente[i] + "')");
-                        cmd3.Connection = con;
-                        cmd3.ExecuteReader();
-                    }
+                   
                 //    con.Open();
 
                //     for (int i = 0; i < ingrediente.Length;++i) {
